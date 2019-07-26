b2b219e2c53f65b755d19e14f6b10ea475907c1e
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2019-07-26 11:29:10 +0200

    Plot distributions with fixed parameters

---
+++ distributions.js
@@ -0,0 +1,79 @@
+function shapeGompertz(MinimalAge, Scale){
+  return(MinimalAge/(Scale * Math.exp(Scale*MinimalAge)));
+}
+function scaleGompertz(MaximalAge, MinimalAge, FailureRateAtMaximalAge, FailureRateAtMinimalAge){
+  return(Math.log(FailureRateAtMaximalAge/FailureRateAtMinimalAge)/(MaximalAge-MinimalAge));
+}
+function shapeWeibull(FailureRateAtMinimalAge, FailureRateAtMaximalAge, MinimalAge, MaximalAge){
+  return(1+Math.log(FailureRateAtMinimalAge/FailureRateAtMaximalAge)/Math.log(MinimalAge/MaximalAge));
+}
+function scaleWeibull(MinimalAge, Shape, FailureRateAtMinimalAge){
+  return(MinimalAge * Math.pow(Shape/(MinimalAge*FailureRateAtMinimalAge), 1/Shape));
+}
+function densityWeibull(Shape, Scale, x){
+  return(Shape/Scale * Math.pow(x/Scale, Shape - 1) * Math.exp(-Math.pow(x/Scale, Shape)));
+}
+function densityGompertz(Shape, Scale, x){
+  return(Scale*Shape*Math.exp(Scale*x)*Math.exp(Shape)*Math.exp(-Shape*Math.exp(Scale*x)));
+}
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
+
+function updateGompertzGraph(){
+  var MinimalAge = 30;
+  var MaximalAge = 100;
+  var FailureRateAtMinimalAge = 0.01;
+  var FailureRateAtMaximalAge = 0.9;
+  
+  Scale = scaleGompertz(MaximalAge, MinimalAge, FailureRateAtMaximalAge, FailureRateAtMinimalAge);
+  Shape = shapeGompertz(MinimalAge, Scale);
+  
+  var listX = [];
+  var listY = [];
+  for (var i = 0; i <= 5; i+=0.01) {
+      listX.push(i);
+      listY.push(densityGompertz(Shape, Scale, i));
+  }
+  
+  
+  var trace1 = {
+    x: listX,
+    y: listY,
+    type: 'scatter'
+  };
+  var data = [trace1];
+      
+  Plotly.newPlot('GompertzGraph', data, {title: 'Gompertz Distribution'}, {showSendToCloud: false});
+}
+
+function updateWeibullGraph(){
+  var MinimalAge = 30;
+  var MaximalAge = 100;
+  var FailureRateAtMinimalAge = 0.01;
+  var FailureRateAtMaximalAge = 0.9;
+  
+  Scale = scaleWeibull(MaximalAge, MinimalAge, FailureRateAtMaximalAge, FailureRateAtMinimalAge);
+  Shape = shapeWeibull(MinimalAge, Scale);
+  
+  var listX = [];
+  var listY = [];
+  for (var i = 0; i <= 5; i+=0.01) {
+      listX.push(i);
+      listY.push(densityWeibull(Shape, Scale, i));
+  }
+  
+
+  var trace1 = {
+    x: listX,
+    y: listY,
+    type: 'scatter'
+  };
+  
+  var data = [trace1];
+      
+  Plotly.newPlot('WeibullGraph', data, {title: 'Weibull Distribution'}, {showSendToCloud: false});
+}
+
+updateWeibullGraph();
+updateGompertzGraph();
